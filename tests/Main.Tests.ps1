13907e859233d4cae83b20803d208299e51ae517
Aaron Parker <aaron@stealthpuppy.com>
2025-10-18 11:53:58 +1100

    Fix grammar in test file SuppressMessage justification
    Corrects the justification text in Diagnostics.CodeAnalysis.SuppressMessageAttribute from 'This OK for the tests files.' to 'This is OK for the test files.' across all test scripts for improved clarity and grammar.

--- tests/Main.Tests.ps1
+++ tests/Main.Tests.ps1
@@ -3,7 +3,7 @@
         Main Pester function tests.
 #>
 [OutputType()]
-[Diagnostics.CodeAnalysis.SuppressMessageAttribute("PSUseDeclaredVarsMoreThanAssignments", "", Justification="This OK for the tests files.")]
+[Diagnostics.CodeAnalysis.SuppressMessageAttribute("PSUseDeclaredVarsMoreThanAssignments", "", Justification="This is OK for the test files.")]
 param ()
 
 BeforeDiscovery {
--- tests/Public/Get-EvergreenApp.Tests.ps1
+++ tests/Public/Get-EvergreenApp.Tests.ps1
@@ -3,7 +3,7 @@
         Public Pester function tests.
 #>
 [OutputType()]
-[Diagnostics.CodeAnalysis.SuppressMessageAttribute("PSUseDeclaredVarsMoreThanAssignments", "", Justification = "This OK for the tests files.")]
+[Diagnostics.CodeAnalysis.SuppressMessageAttribute("PSUseDeclaredVarsMoreThanAssignments", "", Justification = "This is OK for the test files.")]
 [Diagnostics.CodeAnalysis.SuppressMessageAttribute("PSAvoidUsingWriteHost", "", Justification = "Outputs to log host.")]
 param ()
 
